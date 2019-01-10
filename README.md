08c2c25b82890241164ff94b3fcabe578e2657fc
Mo Zhou <cdluminate@gmail.com>
2019-01-10 23:46:42 +0000

    support slug(uuid, sha1)

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ read code or read them in REPL's help mode.
 ```
 ./dh_julia.jl slug data/JSON/Package.toml data/JSON/Versions.toml
 ./dh_julia.jl slug data/JSON/Package.toml data/JSON/Versions.toml 0.20.0
+./dh_julia.jl slug 682c06a0-de6a-54ab-a142-c8b1cf79cde6 1f7a25b53ec67f5e9422f1f551ee216503f4a0fa
 ./dh_julia.jl gather_stdlib_manifest
 ./dh_julia.jl distro_project data/JSON/Package.toml
 ./dh_julia.jl distro_project data/JSON/Package.toml custom_destination
--- src/DistroHelper.jl
+++ src/DistroHelper.jl
@@ -16,12 +16,17 @@ slug("Package.toml", "Versions.toml")
 ```
 """
 function slug(package::AbstractString, versions::AbstractString, version::Any = nothing)
-	Package  = Pkg.TOML.parsefile(package)
-	Versions = Pkg.TOML.parsefile(versions)
-	latest_ver = maximum(VersionNumber.(keys(Versions)))
-	version = string((version == nothing) ? latest_ver : version)
-	uuid = Base.UUID(Package["uuid"])
-	sha1 = Base.SHA1(hex2bytes(Versions[version]["git-tree-sha1"]))
+	if !any(map(isfile, (package, versions)))
+		uuid = Base.UUID(package)
+		sha1 = Base.SHA1(hex2bytes(versions))
+	else
+		Package  = Pkg.TOML.parsefile(package)
+		Versions = Pkg.TOML.parsefile(versions)
+		latest_ver = maximum(VersionNumber.(keys(Versions)))
+		version = string((version == nothing) ? latest_ver : version)
+		uuid = Base.UUID(Package["uuid"])
+		sha1 = Base.SHA1(hex2bytes(Versions[version]["git-tree-sha1"]))
+	end
 	return Base.version_slug(uuid,sha1)
 end
 
