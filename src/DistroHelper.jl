5c6e889fc817f2778c90c53a502b92e6ec0af5bd
Mo Zhou <cdluminate@gmail.com>
2019-01-10 05:58:42 +0000

    add depends() and dedup slug()

--- src/DistroHelper.jl
+++ src/DistroHelper.jl
@@ -4,30 +4,40 @@ using Pkg
 
 """
 Return the slug string for the given package in specific version.
+The latest version will be used when version is omitted.
 
 ```
 slug("Package.toml", "Versions.toml", "0.20.0")
+slug("Package.toml", "Versions.toml")
 ```
 """
-function slug(package::AbstractString, versions::AbstractString, version::AbstractString)
+function slug(package::AbstractString, versions::AbstractString, version::Any = nothing)
 	Package  = Pkg.TOML.parsefile(package)
 	Versions = Pkg.TOML.parsefile(versions)
+	latest_ver = maximum(VersionNumber.(keys(vers)))
+	version = (version == nothing) ? latest_ver : version
 	uuid = Base.UUID(Package["uuid"])
 	sha1 = Base.SHA1(hex2bytes(Versions[version]["git-tree-sha1"]))
 	return Base.version_slug(uuid,sha1)
 end
 
+
 """
-Return the slug string for the latest version of the given package.
+Return the dependencies for a specific version
 
 ```
-slug("Package.toml", "Versions.toml")
+depends("Deps.toml", "0.20.0")
+depends("Deps.toml")
 ```
 """
-function slug(package::AbstractString, versions::AbstractString)
-	vers = Pkg.TOML.parsefile(versions)
-	latest_ver = maximum(VersionNumber.(keys(vers)))
-	return slug(package, versions, string(latest_ver))
+function depends(deps::AbstractString, version::Any = nothing)
+	alldeps = Pkg.TOML.parsefile(deps)
+	bounds = map(x -> VersionNumber.(x), split.(keys(alldeps), "-"))
+	ver	= (version == nothing) ? maximum(maximum(bounds)) : VersionNumber(version) 
+	mask = [ver >= x[1] && ver <= x[2] for x in bounds]
+	for (k, v) in zip(mask, values(alldeps))
+		if k for (i,j) in v println(i, " = ", repr(j)) end end
+	end
 end
 
 end # module
